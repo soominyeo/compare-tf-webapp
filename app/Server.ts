4218b82a2cfa98faef6636ea0d6e2bc1ec2d7f90
yeosm0108 <>
2021-05-20 21:18:59 +0900

    configure apps

---
+++ app/Server.ts
@@ -0,0 +1,41 @@
+import { Express, Request, Response, Router } from "express";
+import express from "express";
+import path from "path";
+import { allowedNodeEnvironmentFlags } from "process";
+
+export class Server {
+  private app: Express;
+
+  constructor(app: Express) {
+    this.app = app;
+    const apiRouter = express.Router();
+
+    this.app.use(express.static(path.resolve("./") + "/build/frontend"));
+    this.app.use("/api", apiRouter);
+
+    this.app.get("/", (req: Request, res: Response): void => {
+      res.sendFile(path.resolve("./") + "/build/frontend/index.html");
+    });
+
+    apiRouter
+      .route("/tensorflow/:target")
+      .get((req: Request, res: Response): void => {
+        if (req.params.target == "js") {
+          // run javascript here
+        } else if (req.params.target == "python") {
+          // run python script here
+        } else {
+          res.status(404).send(`unknown script '${req.params.target}'`);
+        }
+      });
+    this.app.get("*", (req: Request, res: Response): void => {
+      res.redirect("/");
+    });
+  }
+
+  public start(port: number): void {
+    this.app.listen(port, () =>
+      console.log(`Server listening on port ${port}!`)
+    );
+  }
+}
