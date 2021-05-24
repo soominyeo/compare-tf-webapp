713178e192353f25c2a19d393a9541b574937046
yeosm0108 <>
2021-05-25 03:04:01 +0900

    modify client-side script

--- frontend/src/scripts/gdlr.js
+++ frontend/src/scripts/gdlr.js
@@ -1,8 +1,85 @@
 import * as tf from "@tensorflow/tfjs";
 
 function execute(data) {
+	// console.log(tf.getBackend());
+
+	tf.setBackend("cpu");
+
+	try {
+		const startTime = Date.now();
+
+		tf.disposeVariables();
+		const iteration = data.iteration || 500;
+		const alpha = data.learningRate || 0.001;
+		const initialValue = data.initialValue || 1;
+		const userFunction = data.function || "0";
+		const f = parseUserFunction(userFunction);
+
+		const [opt_X, opt_Y] = gradientDescent(f, iteration, alpha, initialValue);
+
+		const isOptNotFound =
+			tf.logicalOr(opt_X.isNaN(), opt_X.isInf()).dataSync()[0] === 1;
+
+		console.log(
+			opt_X.isNaN().dataSync(),
+			opt_X.isInf().dataSync(),
+			opt_X.dataSync()
+		);
+
+		let center;
+		if (isOptNotFound) {
+			center = 0;
+		} else {
+			center = opt_X.dataSync()[0];
+		}
+
+		const N = data.N || 100;
+		const range = (data.range &&
+			data.range[0] &&
+			data.range[1] &&
+			data.range) || [center - 2, center + 2];
+
+		const { regression, slope, intercept, X, Y } = linearRegression(
+			f,
+			range,
+			N
+		);
+		const x = Array.from(X.dataSync());
+		const graph = Array.from(Y.dataSync());
+		const endTime = Date.now();
+
+		const result = {
+			success: true,
+			graph: {
+				x,
+				y: graph,
+			},
+			optimum: { x: opt_X.dataSync()[0], y: opt_Y.dataSync()[0] },
+			regression: {
+				x,
+				y: regression,
+				slope: slope.dataSync()[0],
+				intercept: intercept.dataSync()[0],
+			},
+			rangeX: { min: Math.floor(x[0]), max: Math.ceil(x[x.length - 1]) },
+			rangeY: {
+				min: Math.floor(Math.min(...graph, ...regression)),
+				max: Math.ceil(Math.max(...graph, ...regression)),
+			},
+			time: endTime - startTime,
+		};
+
+		return result;
+	} catch (error) {
+		return { success: false, error };
+	}
+
 	function parseUserFunction(str) {
-		const coeffs = str.split(/\s+/).map((value) => parseInt(value, 10));
+		const coeffs = str
+			.split(/\s+/)
+			.filter((value) => value !== "")
+			.map((value) => parseInt(value, 10))
+			.reverse();
 		return (number) => {
 			let result = tf.tensor(0);
 			for (let index in coeffs) {
@@ -15,7 +92,7 @@ function execute(data) {
 		};
 	}
 
-	function gradientDescent() {
+	function gradientDescent(f, iteration, alpha, initialValue) {
 		const gradient = tf.grad(f);
 		const gd = (x) => tf.sub(x, tf.mul(alpha, gradient(x)));
 
@@ -25,75 +102,26 @@ function execute(data) {
 		}
 
 		X.print();
-		return X;
+		return [X, f(X)];
 	}
 
-	const startTime = Date.now();
+	function linearRegression(f, range, N) {
+		const X = tf.linspace(...range, N);
+		const Y = f(X);
 
-	tf.disposeVariables();
-	const iteration = data.iteration || 500;
-	const alpha = data.learningRate || 0.01;
-	const initialValue = data.initialValue || 1;
-	const userFunction = data.function || "0";
-	const f = parseUserFunction(userFunction);
+		// (N * Sum(X * Y) - Sum(X) * Sum(Y)) / (N * Sum(X ** 2) - Sum(X) ** 2)
+		const slope = tf.div(
+			tf.sub(tf.mul(N, tf.mul(X, Y).sum()), tf.mul(X.sum(), Y.sum())),
+			tf.sub(tf.mul(N, X.square().sum()), X.sum().square())
+		);
+		// Sum(Y) / N - Sum(X) / N * slope
+		const intercept = tf.sub(Y.sum().div(N), X.sum().div(N).mul(slope));
+		const regression = Array.from(
+			tf.add(tf.mul(slope, X), intercept).dataSync()
+		);
 
-	const opt_X = gradientDescent();
-	const opt_Y = f(opt_X);
-
-	const isOptNotFound =
-		tf.logicalOr(opt_X.isNaN(), opt_X.isInf()).dataSync()[0] === 1;
-
-	console.log(opt_X.dataSync());
-
-	let center;
-	if (isOptNotFound) {
-		center = 0;
-	} else {
-		center = opt_X.dataSync()[0];
+		return { regression, slope, intercept, X, Y };
 	}
-
-	const N = 100;
-	const range = (data.range[0] && data.range[1] && data.range) || [
-		center - 2,
-		center + 2,
-	];
-	const X = tf.linspace(...range, N);
-	const Y = f(X);
-
-	// (N * Sum(X * Y) - Sum(X) * Sum(Y)) / (N * Sum(X ** 2) - Sum(X) ** 2)
-	const slope = tf.div(
-		tf.sub(tf.mul(N, tf.mul(X, Y).sum()), tf.mul(X.sum(), Y.sum())),
-		tf.sub(tf.mul(N, X.square().sum()), X.sum().square())
-	);
-	// Sum(Y) / N - Sum(X) / N * slope
-	const intercept = tf.sub(Y.sum().div(N), X.sum().div(N).mul(slope));
-	const regression = Array.from(tf.add(tf.mul(slope, X), intercept).dataSync());
-	const endTime = Date.now();
-
-	const x = Array.from(X.dataSync());
-	const graph = Array.from(Y.dataSync());
-	const result = {
-		success: true,
-		graph: {
-			x,
-			y: graph,
-		},
-		optimum: { x: opt_X.dataSync()[0], y: opt_Y.dataSync()[0] },
-		regression: {
-			x,
-			y: regression,
-			slope: slope.dataSync()[0],
-			intercept: intercept.dataSync()[0],
-		},
-		rangeX: { min: Math.floor(x[0]), max: Math.ceil(x[x.length - 1]) },
-		rangeY: {
-			min: Math.floor(Math.min(...graph, ...regression)),
-			max: Math.ceil(Math.max(...graph, ...regression)),
-		},
-		time: endTime - startTime,
-	};
-
-	return result;
 }
 
 export default execute;
